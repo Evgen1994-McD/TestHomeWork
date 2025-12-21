3675ff8ef4a3dd298085b12797416f11c56b255d
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-12-21 17:41:04 +0500

    Empty стабильная ветка для домашек

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.testhomework"
+    namespace = "com.example.core"
     compileSdk = 36
 
     defaultConfig {
--- app/src/main/java/com/example/testhomework/task1/NavigationListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.testhomework.task1
-
-interface NavigationListener {
-    fun onNextClicked()
-    fun onPreviousClicked()
-}
-
